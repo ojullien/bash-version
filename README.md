405277d44fb4b8b57a7c48dd2a2f157c040f3287
ojullien <ojullien@users.noreply.github.com>
2020-02-07 13:31:05 +0100

    Initial commit
    Signed-off-by: ojullien <ojullien@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# bash-version
-Output ojullien/bash-* apps' version
+# Bash-Version
+
+ojullien/bash-* apps output simple tool.
+
+*Note: I use this script for my own projects, it contains only the features I need.*
+
+## Table of Contents
+
+[Installation](#installation) | [Features](#features) | [Test](#test) | [Contributing](#contributing) | [License](#license)
+
+## Installation
+
+Requires: a Debian/Ubuntu version of linux and a Bash version ~4.4.
+
+1. [Download a release](https://github.com/ojullien/bash-version/releases) or clone this repository.
+2. Use [scripts/install.sh](scripts/install.sh) to automatically install the application in the /opt/oju/bash project folder.
+3. If needed, add `PATH="$PATH:/opt/oju/bash/bin"` to the .profile files.
+
+## Features
+
+This tool output the version of all ojullien/bash-* apps.
+
+```bash
+Usage: version.sh
+```
+
+## Test
+
+This tool does not need unit test.
+
+## Contributing
+
+Thanks you for taking the time to contribute. Please fork the repository and make changes as you'd like.
+
+As I use these scripts for my own projects, they contain only the features I need. But If you have any ideas, just open an [issue](https://github.com/ojullien/bash-version/issues/new/choose) and tell me what you think. Pull requests are also warmly welcome.
+
+If you encounter any **bugs**, please open an [issue](https://github.com/ojullien/bash-version/issues/new/choose).
+
+Be sure to include a title and clear description,as much relevant information as possible, and a code sample or an executable test case demonstrating the expected behavior that is not occurring.
+
+## License
+
+This project is open-source and is licensed under the [MIT License](LICENSE).
---
+++ scripts/install.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+## -----------------------------------------------------------------------------
+## Linux Scripts.
+## Install the bash-version project into the /opt/oju/bash directory.
+##
+## @package ojullien\bash\scripts
+## @license MIT <https://github.com/ojullien/bash-version/blob/master/LICENSE>
+## -----------------------------------------------------------------------------
+#set -o errexit
+set -o nounset
+set -o pipefail
+
+## -----------------------------------------------------------------------------
+## Current project directory, eg: /opt/Shell/scripts/
+## -----------------------------------------------------------------------------
+readonly m_DIR_REALPATH="$(realpath "$(dirname "$0")")"
+# shellcheck source=/dev/null
+. "${m_DIR_REALPATH}/config.sh"
+# shellcheck source=/dev/null
+. "${m_DIR_REALPATH}/includes.sh"
+
+## -----------------------------------------------------------------------------
+## Includes sources & configuration
+## -----------------------------------------------------------------------------
+# shellcheck source=/dev/null
+. "${m_DIR_SYS}/runasroot.sh"
+# shellcheck source=/dev/null
+. "${m_DIR_SYS}/string.sh"
+# shellcheck source=/dev/null
+. "${m_DIR_SYS}/filesystem.sh"
+# shellcheck source=/dev/null
+. "${m_DIR_SYS}/option.sh"
+# shellcheck source=/dev/null
+. "${m_DIR_SYS}/config.sh"
+
+## -----------------------------------------------------------------------------
+## Help
+## -----------------------------------------------------------------------------
+((m_OPTION_SHOWHELP)) && Option::showHelp && exit 0
+
+## -----------------------------------------------------------------------------
+## bash-sys must exists
+## -----------------------------------------------------------------------------
+if [[ ! -d "${m_DIR_SYS}" ]]; then
+    String::error "bash-sys is not installed on ${m_DIR_SYS}"
+    exit 1
+fi
+
+## -----------------------------------------------------------------------------
+## Start
+## -----------------------------------------------------------------------------
+String::separateLine
+String::notice "Today is: $(date -R)"
+String::notice "The PID for $(basename "$0") process is: $$"
+Console::waitUser
+
+## -----------------------------------------------------------------------------
+## Parse the app options and arguments
+## -----------------------------------------------------------------------------
+declare -i iReturn=1
+
+while (( "$#" )); do
+    case "$1" in
+    -t|--trace)
+        shift
+        String::separateLine
+        Install::trace
+        ;;
+    -r|--remove)
+        shift
+        m_INSTALL_OPTION_REMOVE=1
+        ;;
+    *) # unknown option
+        shift
+        String::separateLine
+        Option::showHelp
+        exit 0
+        ;;
+    esac
+done
+
+Install::run ${m_INSTALL_OPTION_REMOVE}
+Console::waitUser
+
+## -----------------------------------------------------------------------------
+## END
+## -----------------------------------------------------------------------------
+String::notice "Now is: $(date -R)"
+exit ${iReturn}
